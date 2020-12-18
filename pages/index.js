f777bd112e2354a8047aaa6ea0d52fb43a43daba
Renan Santos <renanbh27@gmail.com>
2020-12-18 10:01:57 -0300

    teste5

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-  return <div>
+  return <div href="style.css">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
